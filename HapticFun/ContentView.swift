04812fb6717e7d9f15948c42ef969deac96ab9f1
Bill Calkins <bill@calkinsconsulting.com>
2025-01-15 19:43:56 -0600

    Init commit

--- HapticFun/ContentView.swift
+++ HapticFun/ContentView.swift
@@ -6,16 +6,83 @@
 //
 
 import SwiftUI
+import CoreHaptics
 
 struct ContentView: View {
+    
+    @State private var value1 = false
+    @State private var value2 = false
+    @State private var value3 = false
+    @State private var value4 = false
+    
+    @State var hapticEngine: CHHapticEngine?
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Form {
+            Section(header: Text("Heptic Fun!")) {
+                Button("Heavy and High Impact") {
+                    value1.toggle()
+                }
+                .sensoryFeedback(.impact(weight: .heavy, intensity: 1), trigger: value1)
+                Button("Increase") {
+                    value2.toggle()
+                }
+                .sensoryFeedback(.increase, trigger: value2)
+                
+                Button("Success") {
+                    value3.toggle()
+                }
+                .sensoryFeedback(.success, trigger: value3)
+                
+                Button("Error") {
+                    value4.toggle()
+                }
+                .sensoryFeedback(.error, trigger: value4)
+            }
+            Section(header: Text("Custom Haptic")) {
+                Button("Play custom haptic") {
+                    playCustomHaptic()
+                }
+            }
         }
-        .padding()
+        .task {
+            self.prepareHapticEngine()
+        }
+    }
+    
+    private func prepareHapticEngine() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        
+        do {
+            hapticEngine = try CHHapticEngine()
+            try hapticEngine?.start()
+        } catch {
+            print("There was an error starting the haptic engine: \(error.localizedDescription)")
+        }
+    }
+    
+    private func playCustomHaptic() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        
+        var events = [CHHapticEvent]()
+        
+        for i in stride(from: 0, to: 1, by: 0.3) {
+            print("In stride: \(i)")
+            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(i))
+            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(i))
+            
+            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: i)
+            
+            events.append(event)
+        }
+        
+        do {
+            let pattern = try CHHapticPattern(events: events, parameters: [])
+            let player = try hapticEngine?.makePlayer(with: pattern)
+            try player?.start(atTime: .zero)
+        } catch {
+            print("There was an error playing the haptic pattern: \(error.localizedDescription)")
+        }
     }
 }
 
