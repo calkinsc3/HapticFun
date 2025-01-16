e0933f546b2642d106558d7ab29e9e2dc430ba2c
Bill Calkins <bill@calkinsconsulting.com>
2025-01-15 19:26:16 -0600

    Initial Commit

---
+++ HapticFun/HapticFunApp.swift
@@ -0,0 +1,17 @@
+//
+//  HapticFunApp.swift
+//  HapticFun
+//
+//  Created by Bill Calkins on 1/15/25.
+//
+
+import SwiftUI
+
+@main
+struct HapticFunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
